89dcafb2360b16801b6ab146e7fc18da937374d7
raul <raul.diaz01@estudiant.upf.edu>
2024-02-18 22:23:11 +0100

    Mis notas Raúl

--- fileManager.c
+++ fileManager.c
@@ -8,17 +8,18 @@ void  initialiseFdProvider(FileManager * fm, int argc, char **argv) {
     // Complete the initialisation
     /* Your rest of the initailisation comes here*/
     // my_sem_init(&sem,1)
-    // Inicializar semaforo binario 
+    // Inicializar semaforo binario //RAÚL: No sería mejor inicializar el semaforo en el main e ir haciendo sem_wait/sem_signal?? es eso incorrecto??
+    //RAÚL my_sem_wait(&sem) 
     fm->nFilesTotal = argc -1;
-    fm->nFilesRemaining = fm->nFilesTotal;
+    fm->nFilesRemaining = fm->nFilesTotal; //argc -1 es mas eficiente xD
     // Initialise enough memory to  store the arrays
     fm->fdData = malloc(sizeof(int) * fm->nFilesTotal);
     fm->fdCRC= malloc(sizeof(int) * fm->nFilesTotal);
     fm->fileFinished = malloc(sizeof(int) * fm->nFilesTotal);
-    fm->fileAvailable = malloc(sizeof(int) * fm->nFilesTotal);
+    fm->fileAvailable = malloc(sizeof(int) * fm->nFilesTotal);//CORRECTISIMO
 
-    int i;
-    for (i = 1; i < fm->nFilesTotal +1; ++i) {
+    
+    for (int i = 1; i < fm->nFilesTotal +1; ++i) {
         char path[100];
         strcpy(path, argv[i]);
         strcat(path, ".crc");
@@ -37,8 +38,10 @@ void  initialiseFdProvider(FileManager * fm, int argc, char **argv) {
 
         printf("%d - %d | %d - %d | %s\n", fm->fileAvailable[i], fm->fileFinished[i], fm->fdData[i], fm->fdCRC[i], strerror(errno));
     }
+    //RAÚL my_sem_signal(&sem)
 }
 void  destroyFdProvider(FileManager * fm) {
+    //aquí no hay que poner un semaforo??
     int i;
     for (i = 0; i < fm->nFilesTotal; i++) {
         close(fm->fdData[i]);
@@ -52,6 +55,8 @@ int getAndReserveFile(FileManager *fm, dataEntry * d) {
     // This function needs to be implemented by the students
     int i;
     // Semaforo antes del if pero despues del for. Signal antes del return 0 y, por si no entra, despues del if pero antes de terminar el for
+    //RAÚL: yo lo pondría antes del for xq en la condición accedes a fm->nFilesTotal
+    //RAÚL my_sem_wait(&sem)
     for (i = 0; i < fm->nFilesTotal; ++i) {
         if (fm->fileAvailable[i] && !fm->fileFinished[i]) {
             d->fdcrc = fm->fdCRC[i];
@@ -60,15 +65,18 @@ int getAndReserveFile(FileManager *fm, dataEntry * d) {
 
             // You should mark that the file is not available 
             fm->fileAvailable[i] = 0;
-
+            //RAÚL: my_sem_signal(&sem)
             return 0;
         }
+        //RAÚL: my_sem_signal(&sem)
     }             
     return 1;
 }
 void unreserveFile(FileManager *fm,dataEntry * d) {
     // sem 
+    //RAÚL my_sem_wait(&sem)
     fm->fileAvailable[d->index] = 1; 
+    //RAÚL: my_sem_signal(&sem)
     // sem
 }
 
@@ -78,5 +86,7 @@ void markFileAsFinished(FileManager * fm, dataEntry * d) {
     if (fm->nFilesRemaining == 0) {
         printf("All files have been processed\n");
         //TO COMPLETE: unblock all waiting threads, if needed
+        //RAÚL: No entiendo. Cada thread tiene que tener un semaforo? Como desbloqueamos un thread concretamente?
+        //RAÚL: Supongo que será algo de monitores
     }
 }
